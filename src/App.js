af1f6f595b95465cda2a8ffd8cbf6d1d1c7d6a44
vivekcodzz <vivekcodzz@gmail.com>
2023-06-11 15:29:48 +0530

    up-check

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p>
+          Building soon  😒😒
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
